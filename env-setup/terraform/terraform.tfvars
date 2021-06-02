46fe530e6157124debf02be7ebfa6d424ae31a15
jarokaz <jarokaz@no-reply.com>
2021-06-02 23:48:35 +0000

    updates

---
+++ env-setup/terraform/terraform.tfvars
@@ -0,0 +1,11 @@
+project_id = "jk-vertex-ai-test"
+subnet_region = "us-central1"
+zone = "us-central1-a"
+name_prefix = "jk-vertex-env"
+machine_type = "n1-standard-8"
+gpu_type = "NVIDIA_TESLA_T4"
+gpu_count = 1
+install_gpu_driver = true
+image_family = "tf2-2-4-cu11"
+
+
---
+++ env-setup/terraform/variables.tf
@@ -0,0 +1,92 @@
+
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+variable "project_id" {
+    description = "The GCP project ID"
+    type        = string
+}
+
+variable "gcs_region" {
+    description = "The region for the GCS bucket"
+    type        = string
+    default     = null
+}
+
+variable "zone" {
+    description = "The zone for a Vertex Notebook instance"
+    type        = string
+}
+
+variable "name_prefix" {
+    description = "The name prefix to add to the resource names"
+    type        = string
+}
+
+variable "machine_type" {
+    description = "The Notebook instance's machine type"
+    type        = string
+}
+
+variable "network_name" {
+  description = "The network name for the Notebook instance"
+  type        = string
+  default     = "default"
+}
+
+variable "subnet_name" {
+  description = "The subnet name for the Notebook instance"
+  type        = string
+  default     = "default"
+}
+
+variable "subnet_region" {
+    description = "The region for the Notebook subnet"
+    type        = string
+}
+
+variable "boot_disk_size" {
+    description = "The size of the boot disk"
+    default     = 200
+}
+
+variable "image_family" {
+    description = "A Deep Learning image family for the Notebook instance"
+    type        = string
+}
+
+variable "gpu_type" {
+    description = "A GPU type for the Notebook instance"
+    type        = string
+    default     = null
+}
+
+variable "gpu_count" {
+    description = "A GPU count for the Notebook instance"
+    type        = string
+    default     = null
+}
+
+variable "install_gpu_driver" {
+    description = "Whether to install GPU driver"
+    type        = bool
+    default     = true
+}
+
+variable "force_destroy" {
+    description = "Whether to remove the bucket on destroy"
+    type        = bool
+    default     = false
+}
